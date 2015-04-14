0d85f49c2d6c53a2d3c69bc0a63d448f327e268e
Venkat <Venkat@MacBook-Pro.local>
2015-04-13 17:46:24 -0700

    Initial Commit

---
+++ UITableViewControllerChallenge/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  UITableViewControllerChallenge
+//
+//  Created by Venkat on 4/13/15.
+//  Copyright (c) 2015 eTouch. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ UITableViewControllerChallenge/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  UITableViewControllerChallenge
+//
+//  Created by Venkat on 4/13/15.
+//  Copyright (c) 2015 eTouch. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
